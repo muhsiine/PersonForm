4b23da627709a368e1d65f1573c68ec1941d7968
IhssaneAZ <ihssanaouladzerouali@gmail.com>
2022-03-30 16:41:18 +0000

    Added controller tests

--- src/main/java/com/forms/personform/controller/PersonController.java
+++ src/main/java/com/forms/personform/controller/PersonController.java
@@ -5,8 +5,8 @@ import com.forms.personform.entities.Person;
 import com.forms.personform.service.PersonSrv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,15 +20,15 @@ public class PersonController {
     @Autowired
     private PersonSrv personService;
 
-    @GetMapping("/listPeople")
-    public String index(Model model, @RequestParam(name="page",defaultValue = "0")int page){
+    @GetMapping(value = "/listPeople")
+    public String index(ModelMap model){
         List<Person> peoplePages = personService.getPeople();
         model.addAttribute("listPeople",peoplePages);
 
         return "people";
     }
     @GetMapping("/search")
-    public String search(Model model, @RequestParam(name="page",defaultValue = "0")int page,
+    public String search(ModelMap model,
                         @RequestParam(name="keyword",defaultValue = "")String kw){
         List<Person> peoplePages = personService.findByKeyword(kw);
         model.addAttribute("listPeople",peoplePages);
@@ -39,7 +39,7 @@ public class PersonController {
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public String delete(Model model,@PathVariable Long id,@RequestParam(name="page",defaultValue = "0")int page){
+    public String delete(ModelMap model,@PathVariable Long id){
         personService.delete(id);
         List<Person> peoplePages = personService.getPeople();
         model.addAttribute("listPeople",peoplePages);
@@ -52,12 +52,10 @@ public class PersonController {
     }
 
     @PostMapping("/save")
-    public String save(ModelMap model, Person person,@RequestParam(name="page",defaultValue = "0")int page,
-                       @RequestParam(name="keyword",defaultValue = "")String kw){
+    public String save(ModelMap model, Person person){
         personService.save(person);
-        List<Person> peoplePages = personService.findByKeyword(kw);
+        List<Person> peoplePages = personService.getPeople();
         model.addAttribute("listPeople",peoplePages);
-        model.addAttribute("keyword",kw);
         return LIST_PEOPLE_FRAGMENT;
     }
 }
---
+++ src/test/java/com/forms/personform/controller/PersonControllerTest.java
@@ -0,0 +1,143 @@
+package com.forms.personform.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.forms.personform.entities.Person;
+import com.forms.personform.service.PersonSrv;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@ExtendWith(MockitoExtension.class)
+class PersonControllerTest {
+
+    private MockMvc mockMvc;
+    private String keyword = "ih";
+    private List<Person> peopleList = new ArrayList<>();
+
+    @Mock
+    private PersonSrv personService;
+
+    @InjectMocks
+    private PersonController personController;
+
+    @BeforeEach
+    public void setup(){
+        this.mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+
+        peopleList.add(new Person(1L,"Janih","Smith", LocalDate.now(),"Avenue XXX"));
+        peopleList.add(new Person(2L,"Iharu","Watson", LocalDate.now(),"Avenue YYY"));
+        peopleList.add(new Person(74L,"Sih","Williams", LocalDate.now(),"Avenue ZZZ"));
+
+
+    }
+
+    @Test
+    void index() throws Exception {
+
+        when(personService.getPeople()).thenReturn(peopleList);
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/listPeople")
+        )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML_VALUE));
+                .andExpect(MockMvcResultMatchers.view().name("people"))
+                .andExpect(model().attributeExists("listPeople"))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("id"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("firstname"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("lastname"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("birthday"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("address"))));
+
+    }
+
+    @Test
+    void search() throws Exception {
+
+        when(personService.findByKeyword(keyword)).thenReturn(peopleList);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/search")
+                                .param("keyword", keyword)
+                )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("listPeople"))
+                .andExpect(model().attributeExists("keyword"))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("firstname",containsStringIgnoringCase(keyword)))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("lastname"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("birthday"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("address"))));
+    }
+
+
+
+    @Test
+    void delete() throws Exception {
+        Long id = 5L;
+
+        when(personService.getPeople()).thenReturn(peopleList);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.delete("/delete/{id}",id)
+                ).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("listPeople"))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("id",not(equalTo(id))))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("firstname"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("lastname"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("birthday"))))
+                .andExpect(model().attribute("listPeople",everyItem(hasProperty("address"))));
+            }
+
+    @Test
+    void save() throws Exception {
+        Person p = new Person(1L,"John","Smith", null,"Avenue XXX");
+
+       ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestForm=ow.writeValueAsString(p);
+        System.out.println("-----------"+requestForm);
+
+        when(personService.getPeople()).thenReturn(peopleList);
+        //Gives stubbing problem
+        when(personService.save(Mockito.any())).thenReturn(p);
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/save")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestForm)
+                )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("listPeople"));
+
+    }
+}
